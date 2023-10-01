820f67d389c74cb1da74f20bc29451a569177029
Antonio José Masiá <email@antoniomasia.com>
2023-10-01 18:56:57 +0200

    clean neo-tree config

--- lua/ajmasia/plugins/nvim_neo_tree.lua
+++ lua/ajmasia/plugins/nvim_neo_tree.lua
@@ -7,35 +7,35 @@ return {
 			"nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim",
 		},
-		keys = {
-			{
-				"<leader>e",
-				function()
-					require("neo-tree.command").execute({ toggle = true, reveal_force_cwd = true })
-				end,
-				desc = "Explorer NeoTree (root dir)",
-			},
-			{
-				"<leader>E",
-				function()
-					require("neo-tree.command").execute({ action = "close" })
-				end,
-				desc = "Explorer NeoTree (root dir)",
-			},
-			{
-				"<leader>ge",
-				function()
-					require("neo-tree.command").execute({ toggle = true, source = "git_status" })
-				end,
-				desc = "Explorer NeoTree (root dir)",
-			},
-			{
-				"<leader>be",
-				function()
-					require("neo-tree.command").execute({ toggle = true, source = "buffers" })
-				end,
-				desc = "Explorer NeoTree (root dir)",
-			},
-		},
+		-- keys = {
+		-- 	{
+		-- 		"<leader>e",
+		-- 		function()
+		-- 			require("neo-tree.command").execute({ toggle = true, reveal_force_cwd = true })
+		-- 		end,
+		-- 		desc = "Explorer NeoTree (root dir)",
+		-- 	},
+		-- 	{
+		-- 		"<leader>E",
+		-- 		function()
+		-- 			require("neo-tree.command").execute({ action = "close" })
+		-- 		end,
+		-- 		desc = "Explorer NeoTree (root dir)",
+		-- 	},
+		-- 	{
+		-- 		"<leader>ge",
+		-- 		function()
+		-- 			require("neo-tree.command").execute({ toggle = true, source = "git_status" })
+		-- 		end,
+		-- 		desc = "Explorer NeoTree (root dir)",
+		-- 	},
+		-- 	{
+		-- 		"<leader>be",
+		-- 		function()
+		-- 			require("neo-tree.command").execute({ toggle = true, source = "buffers" })
+		-- 		end,
+		-- 		desc = "Explorer NeoTree (root dir)",
+		-- 	},
+		-- },
 	},
 }
