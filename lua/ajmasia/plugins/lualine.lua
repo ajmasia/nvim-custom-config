563bcb224ea205a7ad83e4778ad984a20cc33ba4
Antonio José Masiá <email@antoniomasia.com>
2023-10-01 11:22:08 +0200

    add lualine plugin

---
+++ lua/ajmasia/plugins/lualine.lua
@@ -0,0 +1,63 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+		lualine.setup({
+			options = {
+				theme = "auto",
+				icons_enabled = true,
+				globalstatus = true,
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = { "alpha", "dashboard", "TelescopePrompt" },
+					winbar = {
+						"help",
+						"startify",
+						"dashboard",
+						"packer",
+						"neogitstatus",
+						"NvimTree",
+						"Trouble",
+						"alpha",
+						"lir",
+						"Outline",
+						"spectre_panel",
+						"toggleterm",
+						"Scratch",
+					},
+				},
+			},
+			extensions = { "lazy" },
+			sections = {
+				lualine_a = { { "mode", upper = true } },
+				lualine_b = { { "branch", icon = "" }, "db_ui#statusline" },
+				lualine_c = { { "filename", file_status = true, path = 1 } },
+				lualine_x = {
+					"diagnostics",
+					{
+						"diff",
+						symbols = {
+							added = " ",
+							modified = " ",
+							removed = " ",
+						},
+					},
+					{
+						require("ajmasia.utils.lsp").get_attached_servers,
+					},
+					{
+						require("lazy.status").updates,
+						cond = require("lazy.status").has_updates,
+						color = { fg = "ff9e64" },
+					},
+				},
+				lualine_y = { "filetype" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
+}
--- lua/ajmasia/utils/lsp.lua
+++ lua/ajmasia/utils/lsp.lua
@@ -1,8 +1,9 @@
 local M = {}
 
-local lazy_utils = require "lazy.core.util"
+local lazy_utils = require("lazy.core.util")
 
 local diagnostic_enabled = true
+
 M.toggle_diagnostics = function()
 	diagnostic_enabled = not diagnostic_enabled
 	if diagnostic_enabled then
@@ -12,6 +13,61 @@ M.toggle_diagnostics = function()
 		vim.diagnostic.disable()
 		lazy_utils.warn("Disabled diagnostics", { title = "Diagnostics" })
 	end
+end
+
+local function remove_underscores(str)
+	return str:gsub("_", "")
+end
+
+local servers_icons = {
+	inactive = "",
+	luals = "",
+	tsserver = "",
+	cssls = "",
+	emmetls = "",
+	html = "",
+	marksman = "",
+}
+
+M.get_attached_servers = function(msg)
+	msg = msg or ""
+
+	---@diagnostic disable-next-line: missing-parameter, deprecated
+	local buf_clients = vim.lsp.buf_get_clients()
+
+	if next(buf_clients) == nil then
+		if type(msg) == "boolean" or #msg == 0 then
+			return ""
+		end
+
+		return msg
+	end
+
+	local buf_client_names = {}
+	local copilot_active = false
+
+	-- add client
+	for _, client in pairs(buf_clients) do
+		if client.name ~= "copilot" then
+			local client_name = remove_underscores(client.name)
+
+			table.insert(buf_client_names, servers_icons[client_name])
+		end
+
+		-- if client.name == "copilot" then
+		-- 	copilot_active = true
+		-- end
+	end
+
+	local unique_client_names = vim.fn.uniq(buf_client_names)
+	local language_servers = table.concat(unique_client_names, " ") .. " "
+
+	-- copilot status
+	-- if copilot_active then
+	-- 	language_servers = language_servers .. "%#SLCopilot#" .. " " .. "" .. " %*"
+	-- end
+
+	return language_servers
 end
 
 return M
