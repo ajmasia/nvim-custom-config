83f137f591e6357e3fb6767789af25cb46cbedb0
Antonio José Masiá <email@antoniomasia.com>
2023-10-01 09:47:33 +0200

    update textobjects config from treesitter

--- lua/ajmasia/plugins/treesitter.lua
+++ lua/ajmasia/plugins/treesitter.lua
@@ -44,14 +44,37 @@ return {
 				lookahead = true,
 				keymaps = {
 					-- this keymap permit select objects based on tree-sitter parser
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@conditional.outer",
-					["ic"] = "@conditional.inner",
-					["al"] = "@loop.outer",
-					["il"] = "@loop.inner",
-					["ag"] = "@comment.outer",
-					["ig"] = "@comment.outer",
+					["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment region" },
+					["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment region" },
+
+					["a:"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/field region" },
+					["i:"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/field region" },
+
+					["ag"] = { query = "@comment.outer", desc = "Select outer part of a comment" },
+					["ig"] = { query = "@comment.inner", desc = "Select inner part of a comment" },
+
+					["ac"] = { query = "@conditional.outer", desc = "Select outer part of a conditional region" },
+					["ic"] = { query = "@conditional.inner", desc = "Select inner part of a conditional region" },
+
+					["al"] = { query = "@loop.outer", desc = "Select outer part of a loop region" },
+					["il"] = { query = "@loop.inner", desc = "Select inner part of a loop region" },
+
+					["ab"] = { query = "@block.outer", desc = "Select outer part of a block region" }, -- overrides default text object block of parenthesis to parenthesis
+
+					["ib"] = { query = "@block.inner", desc = "Select inner part of a block region" }, -- overrides default text object block of parenthesis to parenthesis
+
+					["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
+					["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
+				},
+				include_surrounding_whitespace = true,
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					["<leader>on"] = "@parameter.inner", -- swap object under cursor with next
+				},
+				swap_previous = {
+					["<leader>op"] = "@parameter.inner", -- swap object under cursor with previous
 				},
 			},
 		},
